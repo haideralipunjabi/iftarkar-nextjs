8df11985d3e122d48dfc5edb57adcddf78dcacc9
Haider Ali Punjabi <haiderali@cyberservices.com>
2021-04-08 21:22:20 +0530

    Reduced CSS

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
   const router = useRouter();
   return (
     <footer className="footer has-text-primary-light">
-      <div className="content has-text-centered">
+      <div className="has-text-centered">
         <a href="https://madeinkashmir.org" target="_blank" rel="noopener noreferrer">Made in Kashmir</a> | <a href="https://haideralipunjabi.com" target="_blank" rel="noopener noreferrer">Haider Ali Punjabi</a> | <a href="https://hackesta.org" target="_blank" rel="noopener noreferrer">Hackesta</a>
       </div>
     </footer>
