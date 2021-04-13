17b3860f3087db01cad0e679dd50d5001ac20ea6
Haider Ali Punjabi <haiderali@cyberservices.com>
2021-04-13 10:25:23 +0530

    Added Iftar Dua 2

--- components/duas.js
+++ components/duas.js
@@ -5,46 +5,68 @@ import Languages from "../data/languages.json";
 import styles from "./duas.module.scss";
 
 export default function Duas() {
-    const [dua,setDua] = useState("");
+    const [duaIdx,setDuaIdx] = useState();
     const router = useRouter();
     const Language = Languages[router.locale];
-    const Duas = {
-      "sahar": {
+    const Duas = [
+      {
+        "titles":{
+          "en":"Dua Sahar",
+          "ur":"دُعاِ سحر",
+          "kmr":"دُعاِ سحر"
+        },
         "arabic": "وَبِصَوْمِ غَدٍ نَّوَيْتُ مِنْ شَهْرِ رَمَضَانَ",
         "english":"I intend to keep the fast for tomorrow in the month of Ramadan",
         "roman":"Wa bisawmi ghadinn nawaiytu min shahri ramadan",
       },
-      "iftar": {
+      {
+        "titles":{
+          "en":"Dua Iftar - 1",
+          "ur":"دُعاِ اِفطار - ۱",
+          "kmr":"دُعاِ اِفطار - ۱"
+        },
         "arabic":"اللَّهُمَّ اِنِّى لَكَ صُمْتُ وَبِكَ امنْتُ وَعَليْكَ تَوَكّلتُ وَعَلى رِزْقِكَ اَفْطَرْتُ",
         "english":"O Allah! I fasted for You and I believe in You and I put my trust in You and I break my fast with Your sustenance",
         "roman":"Allahumma inni laka sumtu wa bika aamantu wa alayka tawakkaltu wa ala rizq-ika-aftartu"
+      },
+      {
+        "titles":{
+          "en":"Dua Iftar - 2",
+          "ur":"دُعاِ اِفطار - ۲",
+          "kmr":"دُعاِ اِفطار - ۲"
+        },
+        "arabic":"ذَهَبَ الظَّمَأُ وَابْتَلَّتِ الْعُرُوقُ وَثَبَتَ الأَجْرُ إِنْ شَاءَ اللَّهُ ",
+        "english":"Thirst has gone, the arteries are moist, and the reward is sure, if Allah wills.",
+        "roman":"dhahabadh dhama-u wabtallatil ‘urooqu, wa tha-batal ajru insha Allah"
       }
-    }
+    ]
   return (
     <>
      <div className="container">
      <div className={classNames("tabs is-centered")}>
         <ul>
           {
-            Object.keys(Duas).map(key=>(
-              <li key={key} className={classNames({"is-active": dua===key})}>
+            Duas.map((dua,key)=>(
+              <li key={key} className={classNames({"is-active": duaIdx===key})}>
                 <a className="is-size-3 is-size-5-mobile" data-target={key} onClick={()=>{
-                  if(dua===key) setDua();
-                  else setDua(key);
-                }}>{Language.dua} {Language[key]}</a>
+                  if(duaIdx===key) setDuaIdx();
+                  else setDuaIdx(key);
+                }}>
+                  {dua.titles[router.locale]}
+                </a>
               </li>
             ))
           }
         </ul>
       </div>
       {
-        Object.keys(Duas).map(key=>(
-          <div key={key} className={classNames("dua","has-text-centered","px-3",{"is-hidden":dua!=key})}>
-            <h2 className="amiri is-size-2 is-size-4-mobile">{Duas[key]["arabic"]}</h2>
+        Duas.map((dua,key)=>(
+          <div key={key} className={classNames("dua","has-text-centered","px-3",{"is-hidden":duaIdx!=key})}>
+            <h2 className="amiri is-size-2 is-size-4-mobile">{dua["arabic"]}</h2>
             <hr className={styles.seperator}/>
-            <h2 className="is-size-4 is-size-6-mobile">{Duas[key]["roman"]}</h2>
+            <h2 className="is-size-4 is-size-6-mobile">{dua["roman"]}</h2>
             <hr className={styles.seperator}/>
-            <h2 className="is-size-4 is-size-6-mobile">{Duas[key]["english"]}</h2>
+            <h2 className="is-size-4 is-size-6-mobile">{dua["english"]}</h2>
           </div>
         ))
       }
