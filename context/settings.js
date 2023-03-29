d9484db5a7cc2dc748de3620ae4f4ccad3e88ec8
Haider Ali Punjabi <haiderali@cyberservices.com>
2023-03-29 17:30:51 +0530

    Testing events

--- context/settings.js
+++ context/settings.js
@@ -47,6 +47,10 @@ export function SettingsWrapper({ children }) {
     }
   }, [settings]);
   const updateSettings = (key, value) => {
+    gtag('event','settings',{
+      'key': key,
+      'value': value
+    });
     localStorage.setItem("settings-" + key, value);
     setSettings((settings) => ({
       ...settings,
